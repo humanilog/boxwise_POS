3ace96a0a3e0706df8f31b13236201cedf97d03f
HaGuesto <h.guertner@humanilog.org>
2018-12-16 12:01:24 +0100

    First commit

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Boxwise Point-of-Sale",
+
+    'summary': """
+    POS for Free Shops
+        """,
+
+    'author': "Humanilog",
+    'website': "www.humanilog.org",
+
+    'category': 'Uncategorized',
+    'version': '11.0.1.0.0',
+
+    'depends': [
+        'pos', 
+    ],
+    'data': []
+}
